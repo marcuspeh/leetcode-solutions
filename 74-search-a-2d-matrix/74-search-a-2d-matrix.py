df53382a935ef3e8eeebe8228d155de54e5c4fa6
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-02-25 23:10:21 +0800

    Time: 47 ms (76.76%), Space: 14.5 MB (56.26%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,41 +1,40 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         row = self.findRow(matrix, target)
+        
+        # If target not in any col
         if row == -1:
             return False
         
-        col = self.findCol(matrix, target, row)
+        return self.findCol(matrix[row], target)
         
-        return col != -1
-        
+    # Find col that target may be in
     def findRow(self, matrix, target):
         start = 0
         end = len(matrix) - 1
         
         while start <= end:
-            mid = (start + end) // 2
+            mid = start + (end- start) // 2
             if matrix[mid][0] <= target <= matrix[mid][-1]:
                 return mid
             elif matrix[mid][0] > target:
                 end = mid - 1
-            else: 
+            else:
                 start = mid + 1
-        
+            
         return -1
     
-    def findCol(self, matrix, target, row):
+    # Find target in col and return if found
+    def findCol(self, arr, target):
         start = 0
-        end = len(matrix[0]) - 1
+        end = len(arr) - 1
         
         while start <= end:
-            mid = (start + end) // 2
-            
-            if matrix[row][mid] < target:
+            mid = start + (end - start) // 2
+            if arr[mid] < target:
                 start = mid + 1
-            elif matrix[row][mid] > target:
+            elif arr[mid] > target:
                 end = mid - 1
             else:
-                return mid
-            
-        return -1
-                
+                return True
+        return False
