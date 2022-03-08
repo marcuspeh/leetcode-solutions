f44678735c9091df7c96624fb5e7ffb8167c2d0a
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-08 21:01:31 +0800

    Time: 56 ms (97.51%), Space: 15.3 MB (37.19%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        container = {}
-        for i, j in enumerate(nums):
-            if target - j in container:
-                return [container[target - j], i]
-            container[j] = i
-        return [-1, -1]
-                
+        cache = {}
+        
+        for i in range(len(nums)):
+            toFind = target - nums[i]
+            
+            if toFind in cache:
+                return [i, cache[toFind]]
+            
+            cache[nums[i]] = i
