55986fca44355f2af9fe495985d0bcb440253486
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-08 22:52:47 +0800

    Time: 995 ms (68.50%), Space: 18.3 MB (20.90%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,25 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        result = []
         nums.sort()
-        for i in range(len(nums) - 2):
-            if i != 0 and nums[i] == nums[i - 1]:
-                continue
-            left = i + 1
-            right = len(nums) - 1
-
-            while left < right:
-                temp = nums[i] + nums[left] + nums[right]
-                if temp == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif temp < 0:
-                    left += 1
+        
+        first = 0
+        
+        while first < len(nums):
+            toFind = -nums[first]
+            
+            second = first + 1
+            third = len(nums) - 1
+            
+            while second < third:
+                temp = nums[second] + nums[third]
+                
+                if temp == toFind:
+                    result.append([nums[first], nums[second], nums[third]])
+                    
+                    while second < third and nums[second] == nums[second + 1]:
+                        second += 1
+                        
+                    while second < third and nums[third] == nums[third - 1]:
+                        third -= 1
+                        
+                    second += 1
+                    third -= 1
+                elif temp < toFind:
+                    second += 1
                 else:
-                    right -= 1
-        return res
+                    third -= 1
+            
+            while first < len(nums) - 1 and nums[first] == nums[first + 1]:
+                first += 1
+            first += 1
+            
+        return result
