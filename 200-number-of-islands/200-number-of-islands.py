46476f68aade1c57d3b83e5a542d3203c458ee0d
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-10 16:35:16 +0800

    Time: 337 ms (79.06%), Space: 16.4 MB (73.70%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,36 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        result = 0
-        
+        count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                # Check if it is land and not visited
-                if grid[i][j] == "1":
-                    self.dfs(i, j, grid)
-                    result += 1
-        
-        return result
+                if grid[i][j] == '1':
+                    count += 1
+                    self.dfs(grid, i, j)
+        return count
     
-    def dfs(self, row, col, grid):
-        frontier = [(row, col)]
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid):
+            return
         
-        possibleMoves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        if j < 0 or j >= len(grid[0]):
+            return 
+    
+        if grid[i][j] != '1':
+            return
         
-        while frontier:
-            i, j = frontier.pop(0)
-            
-            if grid[i][j] != "1":
-                continue
-            
-            grid[i][j] = "2"
-            
-            for moves in possibleMoves:
-                tempI = i + moves[0]
-                tempJ = j + moves[1]
-                
-                if tempI < 0 or tempJ < 0 or tempI >= len(grid) or tempJ >= len(grid[0]):
-                    continue
-                    
-                frontier.append((tempI, tempJ))
-                
-            
+        grid[i][j] = '#'
+        
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i, j + 1)
