5471fe5b7162ccf588b51afeda5feb3e835bbbc5
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-11 16:36:00 +0800

    Time: 251 ms (80.53%), Space: 21 MB (95.72%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -2,14 +2,17 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         temp = [1]
         
-        # prefix product
-        for i in range(1, len(nums)):
-            temp.append(temp[-1] * nums[i - 1])
+        # Prefix
+        for i in range(len(nums) - 1):
+            temp.append(temp[i] * nums[i])
         
-        # Postfix product
+        # Postfix
         right = 1
         for i in range(len(nums) - 1, -1, -1):
-            temp[i] *= right
+            temp[i] = right * temp[i]
             right *= nums[i]
-
+            
         return temp
+            
+        
+            
