00f5811b9852451f8175a7f5b8120a99b299461e
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-17 09:26:41 +0800

    Time: 298 ms (11.36%), Space: 29.6 MB (45.61%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -8,35 +8,34 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         lengthA = 0
         lengthB = 0
-        
-        # Find length of A
-        curr = headA
-        while curr:
-            curr = curr.next
-            lengthA += 1
-            
-        # Find length of B
-        curr = headB
-        while curr:
-            curr = curr.next
-            lengthB += 1
-            
         currA = headA
         currB = headB
         
-        # Align List
-        if lengthA < lengthB:
-            for _ in range(lengthB - lengthA):
-                currB = currB.next
-        elif lengthB < lengthA:
-            for _ in range(lengthA - lengthB):
-                currA = currA.next
+        # Find length of A
+        while currA:
+            lengthA += 1
+            currA = currA.next
         
-        # Check for collision
+        # Find length of B
+        while currB:
+            lengthB += 1
+            currB = currB.next
+        
+        # Align list
+        currA = headA
+        currB = headB
+        if lengthA > lengthB:
+            for i in range(lengthA - lengthB):
+                currA = currA.next
+        elif lengthB > lengthA:
+            for i in range(lengthB - lengthA):
+                currB = currB.next
+        
+        # Go through all the array and find same node
         while currA and currB:
             if currA == currB:
                 return currA
             currA = currA.next
             currB = currB.next
-            
+        
         return None
