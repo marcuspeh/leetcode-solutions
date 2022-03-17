e5cf04efe49030dfbbba5fd9525aa1f062eb4980
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-17 09:27:51 +0800

    Time: 283 ms (16.68%), Space: 30 MB (16.78%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -6,36 +6,17 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        lengthA = 0
-        lengthB = 0
-        currA = headA
-        currB = headB
+        # Option 1 -> Find the length of both then iterate (less space but slower)
+        # Option 2  -> iterate one and store. Iterate another to check (faster but more space)
+        cache = set()
         
-        # Find length of A
-        while currA:
-            lengthA += 1
-            currA = currA.next
-        
-        # Find length of B
-        while currB:
-            lengthB += 1
-            currB = currB.next
-        
-        # Align list
-        currA = headA
-        currB = headB
-        if lengthA > lengthB:
-            for i in range(lengthA - lengthB):
-                currA = currA.next
-        elif lengthB > lengthA:
-            for i in range(lengthB - lengthA):
-                currB = currB.next
-        
-        # Go through all the array and find same node
-        while currA and currB:
-            if currA == currB:
-                return currA
-            currA = currA.next
-            currB = currB.next
-        
-        return None
+        curr = headA
+        while curr:
+            cache.add(curr)
+            curr = curr.next
+            
+        curr = headB
+        while curr:
+            if curr in cache:
+                return curr
+            curr = curr.next
