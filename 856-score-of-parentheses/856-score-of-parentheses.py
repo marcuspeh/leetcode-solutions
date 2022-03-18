5948ccb6239c42dd6302fecea48042008fb186d5
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-18 08:53:36 +0800

    Time: 44 ms (50.19%), Space: 13.9 MB (70.49%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def scoreOfParentheses(self, S):
-        ans = bal = 0
-        for i, x in enumerate(S):
-            if x == '(':
-                bal += 1
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = [0]
+        
+        for i in s:
+            if i == '(':
+                stack.append(0)
             else:
-                bal -= 1
-                if S[i-1] == '(':
-                    ans += 1 << bal
-        return ans
+                temp = stack.pop()
+                stack[-1] += max(2 * temp, 1)
+        return stack.pop()
