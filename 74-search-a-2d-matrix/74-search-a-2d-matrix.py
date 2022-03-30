1b367256cc044cabaf319e922596d59b3c8567ec
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-03-30 08:47:10 +0800

    Time: 102 ms (5.23%), Space: 14.5 MB (46.15%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,40 +1,38 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Find row
         row = self.findRow(matrix, target)
         
-        # If target not in any col
         if row == -1:
             return False
         
-        return self.findCol(matrix[row], target)
+        return self.findTarget(matrix[row], target)
         
-    # Find col that target may be in
     def findRow(self, matrix, target):
         start = 0
-        end = len(matrix) - 1
-        
-        while start <= end:
-            mid = start + (end- start) // 2
-            if matrix[mid][0] <= target <= matrix[mid][-1]:
-                return mid
-            elif matrix[mid][0] > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-            
-        return -1
-    
-    # Find target in col and return if found
-    def findCol(self, arr, target):
-        start = 0
-        end = len(arr) - 1
+        end = len(matrix) -  1
         
         while start <= end:
             mid = start + (end - start) // 2
+            
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return mid
+            elif matrix[mid][-1] < target:
+                start = mid + 1
+            else:
+                end = mid -1
+                
+        return -1
+    
+    def findTarget(self, arr, target):
+        start = 0
+        end = len(arr) - 1
+        
+        while start < end:
+            mid = start + (end - start) // 2
+            
             if arr[mid] < target:
                 start = mid + 1
-            elif arr[mid] > target:
-                end = mid - 1
             else:
-                return True
-        return False
+                end = mid
+        return arr[start] == target
