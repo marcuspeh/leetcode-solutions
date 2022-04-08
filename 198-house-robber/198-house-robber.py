919715519869053ac4d67113ab5f39493ec4d8c3
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-04-08 14:30:50 +0800

    Time: 48 ms (42.52%), Space: 14 MB (23.24%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        a = 0
+        b = 0
+        
+        for i in nums:
+            temp = max(a + i, b)
+            a = b
+            b = temp
+            
+        return b
