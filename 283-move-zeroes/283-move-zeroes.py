7025f70ff2eb609a7b932ca5e29cf037d68c0162
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-05-16 00:13:07 +0800

    Time: 340 ms (22.80%), Space: 15.6 MB (66.59%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,18 +4,11 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         curr = 0
-        pointer = 0
         
-        while pointer < len(nums):
-            while pointer < len(nums) and nums[pointer] == 0:
-                pointer += 1
-            
-            if pointer >= len(nums):
-                break
-            
-            nums[curr] = nums[pointer]
-            curr += 1
-            pointer += 1
+        for n in nums:
+            if n:
+                nums[curr] = n
+                curr += 1
             
         while curr < len(nums):
             nums[curr] = 0
