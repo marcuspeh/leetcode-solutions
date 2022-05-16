31343994d7d146036aea1edeb0b530f7b81974ea
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-05-16 10:38:48 +0800

    Time: 1224 ms (51.47%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-SELECT salesperson.name
-FROM salesperson LEFT JOIN 
-        (orders JOIN company ON orders.com_id = company.com_id and company.name = 'RED')
-        on salesperson.sales_id = orders.sales_id
-WHERE company.name is null;
+SELECT name
+FROM salesperson
+WHERE name NOT IN (
+    SELECT salesperson.name
+    FROM salesperson JOIN orders on salesperson.sales_id = orders.sales_id
+        JOIN company ON orders.com_id = company.com_id
+    WHERE company.name = 'RED'
+    );
