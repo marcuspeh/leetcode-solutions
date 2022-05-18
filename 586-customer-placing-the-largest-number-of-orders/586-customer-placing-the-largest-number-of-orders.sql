681dad93c3ebd4e8ef646767f77b6f5707a21005
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-05-18 23:42:34 +0800

    Time: 467 ms (55.83%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -2,10 +2,8 @@
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
-HAVING count(order_number) = (
+HAVING count(order_number) >= all (
     SELECT count(order_number)
     FROM orders
     GROUP BY customer_number
-    ORDER BY count(order_number) desc
-    LIMIT 1
 )
