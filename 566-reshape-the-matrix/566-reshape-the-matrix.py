f186c8449d61f456322685d382c554f9c111e826
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-05-18 23:17:40 +0800

    Time: 115 ms (55.81%), Space: 14.7 MB (81.26%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,14 +1,15 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        if sum(map(lambda x: len(x), mat)) != r * c:
+        result = []
+        data = []
+        
+        for row in mat:
+            data.extend(row)
+            
+        if len(data) != r * c:
             return mat
         
-        result = []
-        newMat = []
-        for arr in mat:
-            newMat.extend(arr)
-        
         for i in range(r):
-            result.append(newMat[i * c: (i + 1) * c])
-        
+            result.append(data[i * c: (i + 1) * c])
+            
         return result
