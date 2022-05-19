2ee01d0e723c57bd603ce11c3c3ef2d9e7b2d27d
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-05-19 09:28:07 +0800

    Time: 1330 ms (32.17%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,4 +2,3 @@
 SELECT user_id as buyer_id, join_date, count(order_id) as orders_in_2019
 FROM users LEFT JOIN orders ON orders.buyer_id = users.user_id AND YEAR(order_date) = 2019
 GROUP BY user_id, join_date
-
