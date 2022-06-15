d3a1247e67485ca6d8e892e4c4a94b6e23ffe810
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-06-15 10:39:59 +0800

    Time: 838 ms (30.50%), Space: 19.1 MB (27.77%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, A):
+        N = len(A)
+
+        ans = cur = -1 << 32
+        for x in A:
+            cur = x + max(cur, 0)
+            ans = max(ans, cur)
+
+        rightsums = [None] * N
+        rightsums[-1] = A[-1]
+        for i in range(N-2, -1, -1):
+            rightsums[i] = rightsums[i+1] + A[i]
+
+        maxright = [None] * N
+        maxright[-1] = rightsums[-1]
+        for i in range(N-2, -1, -1):
+            maxright[i] = max(maxright[i+1], rightsums[i])
+
+        leftsum = 0
+        for i in range(N-2):
+            leftsum += A[i]
+            ans = max(ans, leftsum + maxright[i+2])
+        return ans
