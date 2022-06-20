abcc0b7b609fbe07ac7d97f1a4647f297d4d1cdd
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-06-20 14:19:11 +0800

    Time: 75 ms (14.10%), Space: 14.2 MB (48.11%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,15 +1,11 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        # 3 4 4 4 4
-        # Kinda like slide window
         result = 0
         curr = 0
-        
-        for i in range(1, len(nums) - 1):
-            if nums[i - 1] - nums[i] == nums[i] - nums[i + 1]:
+        for i in range(len(nums) - 2):
+            if nums[i + 1] - nums[i] == nums[i + 2] - nums[i + 1]:
                 curr += 1
                 result += curr
             else:
                 curr = 0
-                
         return result
