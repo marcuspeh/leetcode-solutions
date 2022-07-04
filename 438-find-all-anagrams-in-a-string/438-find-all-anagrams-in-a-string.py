12a1e5ed401f949a7031d95b399f12b9dc7f21cf
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-07-04 17:55:49 +0800

    Time: 237 ms (43.85%), Space: 15.3 MB (5.04%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -3,55 +3,36 @@ class Solution:
         if len(p) > len(s):
             return []
         
-        lengthP = len(p)
-        
-        # Set up counter to compare
-        counter = {}
+        # Fill cache for words in p
+        cache = {}
         for i in p:
-            if i in counter:
-                counter[i] += 1
-            else:
-                counter[i] = 1
-        
-        # Sliding window
-        for i in range(len(p)):
-            if s[i] in counter:
-                counter[s[i]] -= 1
-                if counter[s[i]] == 0:
-                    del counter[s[i]]
-            else:
-                counter[s[i]] = -1
-                
-        result = []
-        
-        if len(counter) == 0:
-            result.append(0)
-        
-        for i in range(len(s) - lengthP):
-            print(counter)
-            # get rid of letter at left pointer
-            if s[i] in counter:
-                counter[s[i]] += 1
-                if counter[s[i]] == 0:
-                    del counter[s[i]]
-            else:
-                counter[s[i]] = 1
+            self.addToCache(cache, i)
             
-            # Take into acc right pointer
-            if s[i + lengthP] in counter:
-                counter[s[i + lengthP]] -= 1
-                if counter[s[i + lengthP]] == 0:
-                    del counter[s[i + lengthP]]
-            else:
-                counter[s[i + lengthP]] = -1
-                
-            # Check if anagram in new dictionary
-            if len(counter) == 0:
+        # Fill sliding window
+        for i in range(len(p)):
+            self.removeFromCache(cache, s[i])
+        
+        result = [] if cache else [0]
+        
+        for i in range(len(s) - len(p)):
+            self.addToCache(cache, s[i])
+            self.removeFromCache(cache, s[i + len(p)])
+            
+            if not cache:
                 result.append(i + 1)
-                
         return result
             
-            
-                
-                
-        
+    def removeFromCache(self, cache, letter):
+        if letter in cache:
+            cache[letter] -= 1
+            if cache[letter] == 0:
+                del cache[letter]
+        else:
+            cache[letter] = -1
+
+    def addToCache(self, cache, letter):
+        if letter not in cache:
+            cache[letter] = 0
+        cache[letter] += 1
+        if cache[letter] == 0:
+            del cache[letter]
