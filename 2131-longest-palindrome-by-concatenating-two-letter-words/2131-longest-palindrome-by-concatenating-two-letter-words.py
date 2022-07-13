6f6a3f89af14f0972766780f02c198c28e6713db
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-07-13 20:14:20 +0800

    Time: 2253 ms (38.54%), Space: 38.4 MB (54.28%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        cache = {}
+        
+        for word in words:
+            if word not in cache:
+                cache[word] = 0
+            cache[word] += 1
+        print(cache)
+        result = 0
+        isSame = False
+        for key, value in cache.items():
+            temp = self.getOther(key)
+            if temp == key:
+                if value > 1:
+                    result += (value // 2) * 4
+                if not isSame and value % 2:
+                    result += 2
+                    isSame = True
+            elif temp in cache:
+                result += min(value, cache[temp]) * 2
+        
+        return result
+    
+    def getOther(self, word):
+        return f"{word[1]}{word[0]}"
