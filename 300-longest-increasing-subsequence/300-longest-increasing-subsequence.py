e3003db25a27df9f394ad9527c4ecc9249356a9f
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-08-17 01:05:43 +0800

    Time: 176 ms (67.16%), Space: 14.2 MB (83.99%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -3,14 +3,15 @@ class Solution:
         tails = [0] * len(nums)
         size = 0
         for x in nums:
-            i, j = 0, size
-            while i != j:
-                m = (i + j) // 2
-                if tails[m] < x:
-                    i = m + 1
+            left = 0
+            right = size
+            while left < right:
+                mid = left + (right - left) // 2
+                if tails[mid] < x:
+                    left = mid + 1
                 else:
-                    j = m
-            tails[i] = x
-            size = max(i + 1, size)
+                    right = mid
+            tails[left] = x
+            size = max(left + 1, size)
         return size
 
