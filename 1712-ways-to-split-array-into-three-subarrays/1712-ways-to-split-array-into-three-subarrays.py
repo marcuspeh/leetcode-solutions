f3b99776beceb2e550e85d8bd1c04bc1ab1b9515
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-08-22 00:19:25 +0800

    Time: 2169 ms (54.52%), Space: 27.1 MB (91.43%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.py
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.py
@@ -6,7 +6,7 @@ class Solution:
         
         ans = 0
         for i in range(1, len(nums)): 
-            j = bisect_left(prefix, 2*prefix[i])
-            k = bisect_right(prefix, (prefix[i] + prefix[-1])//2)
+            j = bisect_left(prefix, 2 * prefix[i])
+            k = bisect_right(prefix, (prefix[i] + prefix[-1]) // 2)
             ans += max(0, min(len(nums), k) - max(i+1, j))
         return ans % 1_000_000_007
