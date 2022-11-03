36270c569f01042264a6d46146c932763c9e59d6
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-11-04 00:59:47 +0800

    Time: 3030 ms (26.17%), Space: 52.7 MB (33.63%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,9 +1,7 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
         # Acyclic -> nodes with no incoming edge can lead to all states
-        noIncoming = set()
-        for i in range(n):
-            noIncoming.add(i)
+        noIncoming = set(range(n))
         
         for fro, to in edges:
             if to in noIncoming:
