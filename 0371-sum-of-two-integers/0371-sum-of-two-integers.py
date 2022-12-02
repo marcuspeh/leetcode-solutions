171984dfe8be719947a23ebb5faf1871d1429d6c
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-12-02 16:17:05 +0800

    Time: 58 ms (35.56%), Space: 13.9 MB (64.05%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        isBothNegative = a<0 and b<0
+        mask = 0xffffffff
+        add = a ^ b
+        carry = (a & b) << 1
+        
+        while carry != 0:
+            add, carry=(add ^ carry) & mask, ((add & carry) <<1) & mask
+    
+        return ~(add^mask) if isBothNegative else add
