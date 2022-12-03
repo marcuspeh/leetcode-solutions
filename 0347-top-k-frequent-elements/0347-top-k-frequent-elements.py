60267742f7787c7614fe97796a25f4b856e53323
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-12-04 00:20:12 +0800

    Time: 110 ms (91.16%), Space: 18.7 MB (71.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,19 +2,16 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        cache = {}
+        count = {}
         for n in nums:
-            if n not in cache:
-                cache[n] = 0
-            cache[n] += 1
-            
+            if n not in count:
+                count[n] = 0
+            count[n] += 1
+        
         heap = []
-        for n, freq in cache.items():
-            heapq.heappush(heap, (-freq, n))
-            
-        result = []
-        for i in range(k):
-            freq, n = heapq.heappop(heap)
-            result.append(n)
-            
-        return result
+        for num, freq in count.items():
+            heappush(heap, (freq, num))
+            if len(heap) > k:
+                heappop(heap)
+                
+        return map(lambda x: x[1], heap)
