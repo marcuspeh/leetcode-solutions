969d1979d48344fdeeaefe4b2d4bcf492216907e
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2022-12-05 12:13:00 +0800

    Time: 89 ms (29.41%), Space: 14.2 MB (63.46%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -3,30 +3,15 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        if  nums[0] < nums[-1]:
-            return nums[0]
-        
-        # left lower = go left
-        # left higher = return curr
-        # right lower = go right
-        # right higher = go left
-        
-        # start < mid = 
-        
         start = 0
         end = len(nums) - 1
         
         while start < end:
             mid = start + (end - start) // 2
             
-            if nums[mid] < nums[mid - 1]:
-                return nums[mid]
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            
-            if nums[mid] > nums[start]:
+            if nums[mid] > nums[end]:
                 start = mid + 1
             else:
                 end = mid
                 
-        return start
+        return nums[start]
