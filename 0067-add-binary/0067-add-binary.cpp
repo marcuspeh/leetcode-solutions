62fd0702392153720d3cff5c919ddd4a0bfefa13
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2023-03-20 22:07:47 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (65.09%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,48 +1,21 @@
 class Solution {
 public:
-    pair<string, string> add(char a, char b, string carry) {
-        int count = 0;
-        
-        char arr[] = {a, b};
-        for (char s: arr) {
-            if (s == '1') {
-                count++;
-            }
-        }
-        
-        if (carry == "1") {
-            count++;
-        }
-        return {count % 2 ? "1" : "0", count > 1 ? "1" : "0"};
-    }
-    
     string addBinary(string a, string b) {
+        int carry = 0;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
         string result = "";
         
-        int length = min(a.length(), b.length());
-        string carry = "0";
-        int i;
-        for (i = 0; i < length; i++) {
-            pair<string, string> temp = add(a[a.length() - 1 - i], b[b.length() - 1 - i], carry);
-            result = temp.first + result;
-            carry = temp.second;
+        while (i >= 0 || j >= 0 || carry) {
+            int temp = 0;
+            if (i >= 0 && a[i--] == '1') temp++;
+            if (j >= 0 && b[j--] == '1') temp++;
+            if (carry) temp++;
+            
+            result += '0' + temp % 2;
+            carry = temp / 2;
         }
-        
-        for (; i < a.length(); i++) {
-            pair<string, string> temp = add(a[a.length() - 1 - i], '0', carry);
-            result = temp.first + result;
-            carry = temp.second;
-        }
-        
-        for (; i < b.length(); i++) {
-            pair<string, string> temp = add('0', b[b.length() - 1 - i], carry);
-            result = temp.first + result;
-            carry = temp.second;
-        }
-        if (carry == "1") {
-            result = carry + result;
-        }
-        
+        reverse(result.begin(), result.end());
         return result;
     }
 };
