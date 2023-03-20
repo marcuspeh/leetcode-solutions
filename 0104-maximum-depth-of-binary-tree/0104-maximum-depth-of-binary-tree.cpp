528bc57280cbcbcf1f1f1b9ce890ed168a3fc7ef
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2023-03-20 22:17:39 +0800

    Time: 12 ms (42.30%), Space: 18.9 MB (19.68%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,11 +12,9 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (root == nullptr) {
+        if (!root) {
             return 0;
         }
-        
-        
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
