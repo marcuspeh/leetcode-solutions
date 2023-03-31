dd766d2566c46536643c49d58e9d2dc95ae07d87
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2023-03-31 18:07:54 +0800

    Time: 45 ms (41.34%), Space: 19.9 MB (37.00%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+        
+        vector<vector<int>> result;
+        
+        sort(intervals.begin(), intervals.end());
+        
+        result.push_back(intervals[0]);
+        int last = intervals[0][1];
+        
+        for (int i = 1; i < intervals.size(); i++) {
+            vector<int> curr = intervals[i];
+            if (curr[0] <= last) {
+                result[result.size() - 1][1] = max(last, curr[1]);
+                last = max(last, curr[1]);
+            } else {
+                result.push_back(curr);
+                last = curr[1];
+            }
+        }
+        return result;                                 
+                                   
+            
+    }
+    
+    static bool comparator(vector<int> x1, vector<int> x2) {
+        return x1[0] <= x2[0];
+    }
+
+};
