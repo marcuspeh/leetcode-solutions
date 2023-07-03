52ac88b2c103ab3df7fb55b39b7f512293bd1ff3
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2023-07-04 00:36:23 +0800

    Time: 516 ms (5.02%), Space: 17.7 MB (85.32%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,23 +1,18 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        cache1 = set()
-        cache2 = set()
-        freq1 = [0 for i in range(26)]
-        freq2 = [0 for i in range(26)]
+        cache1, count1 = self.getWordAndCount(word1)
+        cache2, count2 = self.getWordAndCount(word2)
         
-        for char in word1:
-            cache1.add(char)
-            freq1[ord(char) - ord("a")] += 1
+        return cache1 == cache2 and count1 == count2
         
-        for char in word2:
-            cache2.add(char)
-            freq2[ord(char) - ord("a")] += 1
-            
-        freq1.sort()
-        freq2.sort()
+    def getWordAndCount(self, word1: str): 
+        cache = set()
+        count = [0 for _ in range(26)]
         
-        return cache1 == cache2 and freq1 == freq2
-       
+        for word in word1:
+            cache.add(word)
+            count[ord(word) - ord('a')] += 1
         
+        count.sort()
         
-            
+        return cache, count
