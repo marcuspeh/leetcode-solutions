3b7077ae59939433a4abe33161909295b4926985
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2024-02-12 00:07:11 +0800

    Time: 21 ms (99.33%), Space: 12.2 MB (80.92%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,27 +3,34 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        
+        int dp[70][70][70];
+        memset(dp, -1, sizeof(dp));
         dp[0][0][n-1] = grid[0][0] + grid[0][n-1];
 
         int ans = 0;
         for(int row = 1; row < m; row++) {
-            for(int j = 0; j < n; j++) { 
-                for(int k = j+1; k < n; k++) { 
+            for(int c1 = 0; c1 < n; c1++) { 
+                for(int c2 = c1 + 1; c2 < n; c2++) { 
                     int highest = INT_MIN;
                     for(int x = -1; x <= 1; x++) { 
                         for(int y = -1; y <= 1; y++) {
-                            int nj = j + x;
-                            int nk = k + y;
-                            if(nj >= 0 && nj < n && nk >= 0 && nk < n) {
-                                int prev = dp[row-1][nj][nk];
-                                if(prev != -1) {
-                                   highest = max(highest, prev + grid[row][j] + (j != k ? grid[row][k] : 0));
-                                }
+                            int prevC1 = c1 + x;
+                            int prevC2 = c2 + y;
+                            if (prevC1 < 0 || prevC1 >= n) {
+                                continue;
+                            }
+                            if (prevC2 < 0 || prevC2 >= n) {
+                                continue;
+                            }
+                            int prev = dp[row-1][prevC1][prevC2];
+                            if(prev != -1) {
+                               highest = max(highest, dp[row-1][prevC1][prevC2]);
                             }
                         }
                     }
-                    dp[row][j][k] = highest;
+                    highest += grid[row][c1] + grid[row][c2];
+                    dp[row][c1][c2] = highest;
                     ans = max(ans, highest);
                 }
             }
