12b20368975d7063b9afebee23aae2a2fdf42cf1
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2024-02-13 14:21:20 +0800

    Time: 2 ms (43.53%), Space: 7.1 MB (38.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,10 +1,8 @@
-class Solution {
-    int curr[101];
-    
+class Solution {    
+    int curr[100];
 public:
     int uniquePaths(int m, int n) {
-        // memset(curr, 1, sizeof(curr));
-        vector<int> curr(n, 1);
+        fill(&curr[0], &curr[100], 1);
     
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
