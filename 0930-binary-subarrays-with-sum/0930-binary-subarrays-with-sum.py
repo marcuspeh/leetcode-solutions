a8376edee2ae659bd582d75f87a970540f3add79
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2024-03-14 13:45:12 +0800

    Time: 206 ms (95.66%), Space: 20.1 MB (56.59%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:       
+        result = 0
+        currSum = 0
+        counter = {0: 1}
+        for num in nums:
+            currSum += num
+            # if currSum == goal:
+            #     result += 1
+                
+            otherN = currSum - goal
+            if otherN in counter:
+                result += counter[otherN]
+                
+            if currSum not in counter:
+                counter[currSum] = 0
+            counter[currSum] += 1
+        return result
