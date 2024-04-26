2c22418bc6963dd4abb860a8a27db4df614900e5
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2024-04-26 23:32:56 +0800

    Time: 219 ms (88.50%), Space: 20 MB (65.04%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,47 +1,25 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-
-        next_min1 = None
-        next_min1_c = None
-        next_min2 = None
-        next_min2_c = None
-        
-        for col in range(n):
-            if next_min1 is None or grid[n - 1][col] <= next_min1:
-                next_min2 = next_min1
-                next_min2_c = next_min1_c
-                next_min1 = grid[n - 1][col]
-                next_min1_c = col
-            elif next_min2 is None or grid[n - 1][col] <= next_min2:
-                next_min2 = grid[n - 1][col]
-                next_min2_c = col
-        
-        # Fill the recursive cases
-        for row in range(n - 2, -1, -1):
-            min1_c = None
-            min2_c = None
-            min1 = None
-            min2 = None
-
-            for col in range(n):
-                if col != next_min1_c:
-                    value = grid[row][col] + next_min1
-                else:
-                    value = grid[row][col] + next_min2
-                
-                if min1 is None or value <= min1:
-                    min2 = min1
-                    min2_c = min1_c
-                    min1 = value
-                    min1_c = col
-                elif min2 is None or value <= min2:
-                    min2 = value
-                    min2_c = col
+        min1, min2 = self.getSmallest2Pair(grid[0])
+        for i in range(1, len(grid)):
+            min1, min2 = self.getSmallest2Pair(grid[i], min1, min2)
             
-            next_min1_c = min1_c
-            next_min2_c = min2_c
-            next_min1 = min1
-            next_min2 = min2
+        return min1[0]
+                
+            
+    def getSmallest2Pair(self, row, prev1=(0, -1), prev2=(0, -1)):
+        min1 = None
+        min2 = None
         
-        return next_min1
+        for col in range(len(row)):
+            if col == prev1[1]:
+                val = row[col] + prev2[0]
+            else:
+                val = row[col] + prev1[0]
+            
+            if min1 is None or val <= min1[0]:
+                min2 = min1
+                min1 = (val, col)
+            elif min2 is None or val <= min2[0]:
+                min2 = (val,  col)
+        return min1, min2
