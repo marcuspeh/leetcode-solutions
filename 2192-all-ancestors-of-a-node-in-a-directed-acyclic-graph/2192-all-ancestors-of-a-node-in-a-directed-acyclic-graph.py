a0aff76e917a5cdac4a7685776c6294562e8e25c
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2024-06-30 11:39:29 +0800

    Time: 475 ms (63.54%), Space: 48.5 MB (23.90%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,32 +1,36 @@
 class Solution:
-    def getAncestors(self, n, edges):
-        # Initialize adjacency list for each node and ancestors list
-        adjacency_list = [[] for _ in range(n)]
-        ancestors = [[] for _ in range(n)]
-
-        # Populate the adjacency list with edges
-        for edge in edges:
-            from_node = edge[0]
-            to_node = edge[1]
-            adjacency_list[from_node].append(to_node)
-
-        # Perform DFS for each node to find all its ancestors
-        for i in range(n):
-            self.find_ancestors_DFS(i, adjacency_list, i, ancestors)
-
-        return ancestors
-
-    # Helper method to perform DFS and find ancestors
-    def find_ancestors_DFS(
-        self, ancestor, adjacency_list, current_node, ancestors
-    ):
-        for child_node in adjacency_list[current_node]:
-            # Check if the ancestor is already added to avoid duplicates
-            if (
-                not ancestors[child_node]
-                or ancestors[child_node][-1] != ancestor
-            ):
-                ancestors[child_node].append(ancestor)
-                self.find_ancestors_DFS(
-                    ancestor, adjacency_list, child_node, ancestors
-                )
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        edgeMap = {}
+        noStart = {i for i in range(n)}
+        for i, j in edges:
+            if j not in edgeMap:
+                edgeMap[j] = []
+            
+            if i in noStart:
+                noStart.remove(i)
+            
+            edgeMap[j].append(i)
+            
+        result = [set() for i in range(n)]
+        for i in noStart:
+            self.dfs(edgeMap, i, result)
+            
+        sortedResult = []
+        for ancestor in result:
+            sortedResult.append(list(sorted(list(ancestor))))
+        
+        return sortedResult
+    
+    def dfs(self, edgeMap, curr, result):
+        if result[curr]:
+            return result[curr].union({curr})
+    
+        currResult = set()
+        if curr in edgeMap:
+            for nextNode in edgeMap[curr]:
+                currResult.update(self.dfs(edgeMap, nextNode, result))
+        
+        result[curr] = currResult
+        return currResult.union({curr})
+        
+        
