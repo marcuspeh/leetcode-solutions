1da06bd7276aae22502f02d68a3b56f913f4cdb7
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2024-07-30 22:51:07 +0800

    Time: 473 ms (38.00%), Space: 17.6 MB (79.00%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,28 +1,16 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        prefixB = []
-        for char in s:
-            if not prefixB:
-                curr = 0
-            else:
-                curr = prefixB[-1]
-            if char == "b":
-                curr += 1
-            prefixB.append(curr)
-        
-        postfixA = []
-        for i in range(len(s) - 1, -1, -1):
-            if not postfixA:
-                curr = 0
-            else:
-                curr = postfixA[-1]
-            if s[i] == "a":
-                curr += 1
-            postfixA.append(curr)
-        postfixA = postfixA[::-1]
-        
-        result = min(prefixB[-1], postfixA[0])
-        for i in range(1, len(s) - 1):
-            result = min(result, postfixA[i] + prefixB[i + 1], postfixA[i] + prefixB[i - 1])
-        return result
-            
+        n = len(s)
+        aCount = sum(1 for ch in s if ch == "a")
+
+        bCount = 0
+        minDeletions = n
+
+        for ch in s:
+            if ch == "a":
+                aCount -= 1
+            minDeletions = min(minDeletions, aCount + bCount)
+            if ch == "b":
+                bCount += 1
+
+        return minDeletions
