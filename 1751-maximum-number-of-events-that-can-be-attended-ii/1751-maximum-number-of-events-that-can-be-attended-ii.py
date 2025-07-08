3e4cc1caf285d3987e561267082554dbb4958f67
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2025-07-08 19:50:16 +0800

    Time: 679 ms (73.62%), Space: 78.5 MB (76.92%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,39 +1,20 @@
-class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length;
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:        
+        events.sort()
+        n = len(events)
+        starts = [start for start, end, value in events]
+        dp = [[-1] * n for _ in range(k + 1)]
         
-        dp = new int[k + 1][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        def dfs(cur_index, count):
+            if count == 0 or cur_index == n:
+                return 0
+            if dp[count][cur_index] != -1:
+                return dp[count][cur_index]
+
+            # Find the nearest available event after attending event 0.
+
+            next_index = bisect_right(starts, events[cur_index][1])
+            dp[count][cur_index] = max(dfs(cur_index + 1, count), events[cur_index][2] + dfs(next_index, count - 1))
+            return dp[count][cur_index]
         
-        return dfs(0, k, events);
-    }    
-    
-    private int[][] dp;
-    private int dfs(int curIndex, int count, int[][] events) {
-        if (count == 0 || curIndex == events.length) {
-            return 0;
-        }
-        if (dp[count][curIndex] != -1) {
-            return dp[count][curIndex];
-        }
-        int nextIndex = bisectRight(events, events[curIndex][1]);
-        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
-        return dp[count][curIndex];
-    }
-    
-    public static int bisectRight(int[][] events, int target) {
-        int left = 0, right = events.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (events[mid][0] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
-    }   
-}
+        return dfs(0, k)
