f46f3a5bdf179b6f9fa5cff6e9ab9c31cd833718
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2025-08-02 01:14:17 +0800

    Time: 7 ms (4.76%), Space: 13.3 MB (21.66%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        for i in range(numRows):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(ans[i - 1][j - 1] + ans[i - 1][j])
+            ans.append(row)
+        return ans
+        
