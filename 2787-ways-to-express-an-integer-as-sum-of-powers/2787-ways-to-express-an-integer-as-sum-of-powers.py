e9438917a4cceb86c40e35c047796f2a31b0ca2d
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2025-08-12 19:34:11 +0700

    Time: 3371 ms (53.85%), Space: 14.7 MB (46.15%) - LeetHub

---
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfWays(self, n, x):
+        """
+        :type n: int
+        :type x: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            val = i**x
+            for j in range(n + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j >= val:
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % MOD
+        return dp[n][n]
