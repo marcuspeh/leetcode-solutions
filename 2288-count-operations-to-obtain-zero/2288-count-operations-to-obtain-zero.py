b4e2918fa8e166536583395164d78a9aa3a93641
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2025-11-09 11:48:52 +0800

    Time: 0 ms (100%), Space: 17.7 MB (81.03%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -1,7 +1,8 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        res = 0  
+        res = 0  # total number of subtraction operations
         while num1 and num2:
+            # each step of the Euclidean algorithm
             res += num1 // num2
             num1 %= num2
             num1, num2 = num2, num1
