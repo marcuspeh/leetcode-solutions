32befde6c24fcf86479c72986ff0eec279867725
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2026-07-01 23:53:34 +0800

    Time: 11 ms (95.21%), Space: 28.2 MB (54%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -3,7 +3,7 @@ class Solution:
         arr.sort()
         ans = 1
         for i in range(1, len(arr)):
-            if arr[i] >= ans + 1:
+            if arr[i] > ans:
                 ans += 1
 
         return ans
