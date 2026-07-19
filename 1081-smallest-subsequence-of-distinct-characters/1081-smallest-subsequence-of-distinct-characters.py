fea71119cf0c858b17ed4941163ced7a4580b63f
Marcus Peh <54053213+marcuspeh@users.noreply.github.com>
2026-07-19 12:07:15 +0800

    Time: 0 ms (100%), Space: 19.3 MB (39.11%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        num = [0] * 26
+        for ch in s:
+            num[ord(ch) - ord("a")] += 1
+
+        stk = []
+        vis = [0] * 26
+        for ch in s:
+            idx = ord(ch) - ord("a")
+            if not vis[idx]:
+                while stk and stk[-1] > ch:
+                    top_idx = ord(stk[-1]) - ord("a")
+                    if num[top_idx] > 0:
+                        vis[top_idx] = 0
+                        stk.pop()
+                    else:
+                        break
+                vis[idx] = 1
+                stk.append(ch)
+            num[idx] -= 1
+
+        return "".join(stk)
